af8856efc596c44045f5a865adab650b88bf9757
Om Prakash Pandey <pandeyomu16.gol@gmail.com>
2024-01-14 02:04:52 +0530

    dashboard controller todos done

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,20 +1,56 @@
-import mongoose from "mongoose"
-import {Video} from "../models/video.model.js"
-import {Subscription} from "../models/subscription.model.js"
-import {Like} from "../models/like.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import mongoose from "mongoose";
+import { Video } from "../models/video.model.js";
+import { Subscription } from "../models/subscription.model.js";
+import { Like } from "../models/like.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { getAllVideos } from "./video.controller.js";
 
 const getChannelStats = asyncHandler(async (req, res) => {
-    // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
-})
+  let userId = req.user._id;
+
+  const totalSubscribers = await Subscription.countDocuments({
+    channel: userId,
+  });
+  const videosStats = await Video.aggregate([
+    {
+      $match: { owner: userId },
+    },
+    {
+      $lookup: {
+        from: "likes",
+        localField: "_id",
+        foreignField: "video",
+        as: "likes",
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        totalVideos: { $sum: 1 },
+        totalLikes: { $sum: { $size: "$likes" } },
+        totalViews: { $sum: "$views" },
+      },
+    },
+  ]);
+
+  const dashboardStats = Object.assign(
+    { totalVideos: 0, totalLikes: 0, totalSubscribers },
+    videosStats?.[0],
+    { _id: userId }
+  );
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, dashboardStats, "Channel stats fetch successfully")
+    );
+});
 
 const getChannelVideos = asyncHandler(async (req, res) => {
-    // TODO: Get all the videos uploaded by the channel
-})
+  req.query.userId = req.user._id;
+  getAllVideos(req, res);
+});
 
-export {
-    getChannelStats, 
-    getChannelVideos
-    }
+export { getChannelStats, getChannelVideos };
